b0895c9985af719d78d4464804928041e4ed3fca
pr0t0typ3-ZeR0 <guilherme2001fernandes@hotmail.com>
2020-10-31 16:08:40 +0000

    Added index.htm and centered clock

--- src/Main.elm
+++ src/Main.elm
@@ -1,25 +1,30 @@
 module Main exposing (..)
 
 import Browser
-import Element exposing (Element, el, width, height, fill, text)
+import Element exposing (Element, centerX, centerY, el, fill, height, text, width)
 import Element.Background as Background
 import Element.Font as Font
-import String
 import Html exposing (Html)
-import Time exposing (utc, toHour, toMinute, toSecond)
+import String
 import Task
+import Time exposing (toHour, toMinute, toSecond, utc)
+
 
 wallpaper : String
-wallpaper = "https://cdnb.artstation.com/p/assets/images/images/030/073/957/large/alena-aenami-out-of-time-1080p.jpg"
+wallpaper =
+    "https://cdnb.artstation.com/p/assets/images/images/030/073/957/large/alena-aenami-out-of-time-1080p.jpg"
+
 
 type alias Model =
     { zone : Time.Zone
     , time : Time.Posix
     }
 
+
 type Msg
     = Tick Time.Posix
     | AdjustTimeZone Time.Zone
+
 
 main =
     Browser.element
@@ -29,13 +34,15 @@ main =
         , subscriptions = subscriptions
         }
 
-init : () -> (Model, Cmd Msg)
+
+init : () -> ( Model, Cmd Msg )
 init _ =
     ( Model Time.utc (Time.millisToPosix 0)
     , Task.perform AdjustTimeZone Time.here
     )
 
-update : Msg -> Model -> (Model, Cmd Msg)
+
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         Tick newTime ->
@@ -48,14 +55,17 @@ update msg model =
             , Cmd.none
             )
 
+
 subscriptions : Model -> Sub Msg
 subscriptions model =
     Time.every 1000 Tick
+
 
 view : Model -> Html Msg
 view model =
     Element.layout []
         (myElement model)
+
 
 myElement : Model -> Element msg
 myElement model =
@@ -64,19 +74,29 @@ myElement model =
         , width fill
         , height fill
         ]
-        (text (timeStr model))
+        (el
+            [ centerX
+            , centerY
+            ]
+            (text (timeStr model))
+        )
+
 
 timeStr : Model -> String
 timeStr model =
-    "[ " ++
-    formatDigits (String.fromInt (toHour model.zone model.time))
-    ++ ":" ++
-    formatDigits (String.fromInt (toMinute model.zone model.time))
-    ++ ":" ++
-    formatDigits (String.fromInt (toSecond model.zone model.time))
-    ++ " ]"
+    "[ "
+        ++ formatDigits (String.fromInt (toHour model.zone model.time))
+        ++ ":"
+        ++ formatDigits (String.fromInt (toMinute model.zone model.time))
+        ++ ":"
+        ++ formatDigits (String.fromInt (toSecond model.zone model.time))
+        ++ " ]"
+
 
 formatDigits : String -> String
 formatDigits s =
-    if String.length s == 1 then "0" ++ s
-    else s
+    if String.length s == 1 then
+        "0" ++ s
+
+    else
+        s
